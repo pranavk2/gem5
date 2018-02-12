e2a85ddc9ad258ace800c99a62967308bf77039b
Pranav Kumar <pranavkumar@utexas.edu>
2018-02-11 18:32:44 -0600

    added some tutorial files
    Change-Id: I6a2a873b2c9c650ffcbe8a953352fcee2fc5f132

--- configs/tutorial/simple.py
+++ configs/tutorial/simple.py
@@ -27,16 +27,11 @@
 #
 # Authors: Jason Power
 
-""" This file creates a single CPU and a two-level cache system.
-This script takes a single parameter which specifies a binary to execute.
-If none is provided it executes 'hello' by default (mostly used for testing)
+""" This file creates a barebones system and executes 'hello', a simple Hello
+World application.
 
-See Part 1, Chapter 3: Adding cache to the configuration script in the
-learning_gem5 book for more information about this script.
-This file exports options for the L1 I/D and L2 cache sizes.
-
-IMPORTANT: If you modify this file, it's likely that the Learning gem5 book
-           also needs to be updated. For now, email Jason <power.jg@gmail.com>
+This config file assumes that the x86 ISA was built.
+See gem5/configs/learning_gem5/part1/simple.py for a general script.
 
 """
 
@@ -44,36 +39,6 @@ IMPORTANT: If you modify this file, it's likely that the Learning gem5 book
 import m5
 # import all of the SimObjects
 from m5.objects import *
-
-# Add the common scripts to our path
-m5.util.addToPath('../../')
-
-# import the caches which we made
-from caches import *
-
-# import the SimpleOpts module
-from common import SimpleOpts
-
-# Set the usage message to display
-SimpleOpts.set_usage("usage: %prog [options] <binary to execute>")
-
-# Finalize the arguments and grab the opts so we can pass it on to our objects
-(opts, args) = SimpleOpts.parse_args()
-
-# get ISA for the default binary to run. This is mostly for simple testing
-isa = str(m5.defines.buildEnv['TARGET_ISA']).lower()
-
-# Default to running 'hello', use the compiled ISA to find the binary
-#binary = 'tests/test-progs/hello/bin/' + isa + '/linux/hello'
-binary = './spectre.out'
-
-# Check if there was a binary passed in via the command line and error if
-# there are too many arguments
-if len(args) == 1:
-    binary = args[0]
-elif len(args) > 1:
-    SimpleOpts.print_help()
-    m5.fatal("Expected a binary to execute as positional argument")
 
 # create the system we are going to simulate
 system = System()
@@ -90,54 +55,32 @@ system.mem_ranges = [AddrRange('512MB')] # Create an address range
 # Create a simple CPU
 system.cpu = TimingSimpleCPU()
 
-# Create an L1 instruction and data cache
-system.cpu.icache = L1ICache(opts)
-system.cpu.dcache = L1DCache(opts)
-
-# Connect the instruction and data caches to the CPU
-system.cpu.icache.connectCPU(system.cpu)
-system.cpu.dcache.connectCPU(system.cpu)
-
 # Create a memory bus, a coherent crossbar, in this case
-system.l2bus = L2XBar()
-
-# Hook the CPU ports up to the l2bus
-system.cpu.icache.connectBus(system.l2bus)
-system.cpu.dcache.connectBus(system.l2bus)
-
-# Create an L2 cache and connect it to the l2bus
-system.l2cache = L2Cache(opts)
-system.l2cache.connectCPUSideBus(system.l2bus)
-
-# Create a memory bus
 system.membus = SystemXBar()
 
-# Connect the L2 cache to the membus
-system.l2cache.connectMemSideBus(system.membus)
+# Hook the CPU ports up to the membus
+system.cpu.icache_port = system.membus.slave
+system.cpu.dcache_port = system.membus.slave
 
-# create the interrupt controller for the CPU
+# create the interrupt controller for the CPU and connect to the membus
 system.cpu.createInterruptController()
+system.cpu.interrupts[0].pio = system.membus.master
+system.cpu.interrupts[0].int_master = system.membus.slave
+system.cpu.interrupts[0].int_slave = system.membus.master
 
-# For x86 only, make sure the interrupts are connected to the memory
-# Note: these are directly connected to the memory bus and are not cached
-if m5.defines.buildEnv['TARGET_ISA'] == "x86":
-    system.cpu.interrupts[0].pio = system.membus.master
-    system.cpu.interrupts[0].int_master = system.membus.slave
-    system.cpu.interrupts[0].int_slave = system.membus.master
-
-# Connect the system up to the membus
-system.system_port = system.membus.slave
-
-# Create a DDR3 memory controller
+# Create a DDR3 memory controller and connect it to the membus
 system.mem_ctrl = DDR3_1600_8x8()
 system.mem_ctrl.range = system.mem_ranges[0]
 system.mem_ctrl.port = system.membus.master
+
+# Connect the system up to the membus
+system.system_port = system.membus.slave
 
 # Create a process for a simple "Hello World" application
 process = Process()
 # Set the command
 # cmd is a list which begins with the executable (like argv)
-process.cmd = [binary]
+process.cmd = ['tests/test-progs/hello/bin/x86/linux/hello']
 # Set the cpu to use the process as its workload and create thread contexts
 system.cpu.workload = process
 system.cpu.createThreads()
